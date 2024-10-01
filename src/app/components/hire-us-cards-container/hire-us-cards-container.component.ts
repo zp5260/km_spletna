2b32a93f080fba3c1f6445a7368378d1d4734e3f
Zala Pregelj <zala.pregelj@medius.si>
2024-10-01 17:33:59 +0200

    hire us page draft

---
+++ src/app/components/hire-us-cards-container/hire-us-cards-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hire-us-cards-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './hire-us-cards-container.component.html',
+  styleUrl: './hire-us-cards-container.component.css'
+})
+export class HireUsCardsContainerComponent {
+
+}
