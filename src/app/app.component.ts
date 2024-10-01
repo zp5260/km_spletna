bbc851f9ad31fded915c2f6a5070a42681f48eac
Zala Pregelj <zala.pregelj@medius.si>
2024-10-01 14:52:15 +0200

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { KmAnimationComponent } from './components/km-animation/km-animation.component'
-import { HeaderComponent } from './components/header/header.component'
-import { ContactUsComponent } from './components/contact-us/contact-us.component'
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {HeaderComponent} from './components/header/header.component'
+import {ContactUsComponent} from './components/contact-us/contact-us.component'
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ContactUsComponent, RouterLink, RouterLinkActive ],
+  imports: [RouterOutlet, HeaderComponent, ContactUsComponent, RouterLink, RouterLinkActive, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
