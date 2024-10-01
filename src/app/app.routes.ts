c10180653c34b0e4c0230ac6d8e5450cbbcdfe7b
Zala Pregelj <zala.pregelj@medius.si>
2024-10-01 17:07:25 +0200

    some styling fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import {HireUsComponent} from "./components/hire-us/hire-us.component";
 
 
 export const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
 //   { path: 'novice', component: NoviceComponent },
-//   { path: 'najemi-nas', component: NajemiNasComponent },
+  { path: 'hire-us', component: HireUsComponent },
   { path: 'contact-us', component: ContactUsComponent },
   { path: '', redirectTo: '/about-us', pathMatch: 'full' }, // privzeta stran
   { path: '**', redirectTo: 'contact-us' } // če je napačna pot
---
+++ src/app/components/hire-us/hire-us.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {KmAnimationComponent} from "../km-animation/km-animation.component";
+
+@Component({
+  selector: 'app-hire-us',
+  standalone: true,
+  imports: [
+    KmAnimationComponent
+  ],
+  templateUrl: './hire-us.component.html',
+  styleUrl: './hire-us.component.css'
+})
+export class HireUsComponent {
+
+}
