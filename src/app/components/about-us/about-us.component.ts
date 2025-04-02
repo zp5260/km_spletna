85668e29c1ecbd90e25d3a05e4103badc9edf595
zala <zalapregeljkp@gmail.com>
2025-04-03 00:15:07 +0200

    added olaii button

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { KmAnimationComponent } from '../km-animation/km-animation.component'
 import {AboutUsFeedbacksComponent} from "../about-us-feedbacks/about-us-feedbacks.component";
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [KmAnimationComponent, AboutUsFeedbacksComponent],
+  imports: [KmAnimationComponent, AboutUsFeedbacksComponent, ButtonModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
