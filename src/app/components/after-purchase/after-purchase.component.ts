cb867a7820e4c090973376ec5b4f3ece1e677149
zala <zalapregeljkp@gmail.com>
2025-06-22 11:29:38 +0200

    after purchase renaming, photos for treasure hunt

---
+++ src/app/components/after-purchase/after-purchase.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-after-purchase',
+  standalone: true,
+  imports: [],
+  templateUrl: './after-purchase.component.html',
+  styleUrl: './after-purchase.component.css'
+})
+export class AfterPurchaseComponent {
+
+}
