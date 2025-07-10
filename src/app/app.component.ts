0bce19b021c76ef3c031dd2dca7d736153eed488
zala <zalapregeljkp@gmail.com>
2025-07-10 16:16:16 +0200

    added NC kviz 17. 7. 2025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './components/header/header.component'
 import {ContactUsComponent} from './components/contact-us/contact-us.component'
+import {TicketsStoreComponent} from './components/tickets-store/tickets-store.component'
 import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ContactUsComponent, RouterLink, RouterLinkActive, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, ContactUsComponent, RouterLink, RouterLinkActive, FooterComponent, TicketsStoreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
